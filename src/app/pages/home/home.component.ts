9f0c8b33dbf733a8d374524204864d4c03e73bf8
Jahnavi Bantupalli <jbantupa@asu.edu>
2019-04-22 12:00:33 -0700

    added colors to tree

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -21,136 +21,9 @@ import {ellipseForce} from 'd3-ellipse-force'
 })
 export class HomeComponent implements OnInit {
 
-    public customers: any[] = [
-      {
-      'customer': 'Alpegru Retail Properties Ltd',
-      'arr': 240779,
-      'assets': ['Oracle Weblogic', 'Demand Classification', 'JDA Collaborate',
-                  'JDA Demand', 'JDA Fulfillment', 'JDA Market Manager', 'JDA Monitor', 'JDA Order Optimization', 'JDA Platform'],
-      'industry': 'Retail',
-      'theatre': 'EMEA',
-      'is_cloud': 'N',
-      'chs': 73
-     },
-      {
-        'customer': 'BrassCraft Manufacturing Co',
-        'arr': '',
-        'assets': [
-          'Demand Planning/Ext. Edition',
-          'Fulfillment - Deployment',
-          'Fulfillment - Material Alloc',
-          'Fulfillment - Material Plan',
-          'Fulfillment - Production',
-          'JDA Collaborate',
-          'JDA Demand',
-          'JDA Fulfillment',
-          'JDA Market Manager',
-          'JDA Platform'
-        ],
-        'theatre': 'NA MFG',
-        'industry': 'Manufacturing',
-        'is_cloud': 'N',
-        'chs': 98,
-        'class': 'class B'
-      },
-      {
-        'customer': 'ABC Fine Wine & Spirits',
-        'arr': '19258',
-        'assets': [
-          'AWR - Deal/Forward Buy GS',
-          'AWR - Events GS',
-          'AWR - Order Analysis GS',
-          'AWR - Order Expedite Management GS',
-          'AWR - Plans GS',
-          'AWR - Projections GS',
-          'AWR - Truck Split GS',
-          'JDA Advanced Warehouse Replenishment GS'
-        ],
-        'theatre': 'NA Retail',
-        'industry': 'Retail',
-        'is_cloud': 'N',
-        'chs': 92,
-        'class': 'class B'
-      },
-      {
-        'customer': 'ABM REXEL',
-        'arr': '11676',
-        'assets': [
-          'AWR - Allocation CS',
-          'AWR - Booking CS',
-          'AWR - Calendar CS',
-          'AWR - Deal/Forward Buy CS',
-          'AWR - Events CS',
-          'AWR - Export Reports to Excel CS',
-          'AWR - Multi-Tier CS',
-          'AWR - Online Forward Buy CS',
-          'AWR - Order Analysis CS',
-          'AWR - Order Expedite Management CS',
-          'AWR - Overstock Transfer CS',
-          'AWR - Plans CS',
-          'AWR - Projections CS',
-          'AWR - Security CS',
-          'JDA Advanced Warehouse Replenishment Client Server'
-        ],
-        'theatre': 'EMEA',
-        'industry': 'Wholesale/Distribution',
-        'is_cloud': 'N',
-        'chs': 58,
-        'class': 'class A'
-      },
-      {
-        'customer': 'Adtran Inc - US',
-        'arr': '67096',
-        'assets': [
-          'Oracle Weblogic',
-          'JDA Factory Planner',
-          'JDA Manufacturing ABPP',
-          'JDA Platform',
-          'JDA Core Infrastructure Services (CIS)',
-          'SCM UI'
-        ],
-        'theatre': 'NA MFG',
-        'industry': 'Manufacturing',
-        'is_cloud': 'N',
-        'chs': 99,
-        'class': 'class B'
-      },
-      {
-        'customer': 'Apoteket AB',
-        'arr': '70548',
-        'assets': [
-          'CKB - Web Publisher',
-          'JDA Category Knowledge Base',
-          'JDA Floor Planning Plus',
-          'JDA Open Access Mobile',
-          'JDA Planogram Converter',
-          'JDA Space Planning Plus'
-        ],
-        'theatre': 'EMEA',
-        'industry': 'Retail',
-        'is_cloud': 'N',
-        'chs': 91,
-        'class': 'class B'
-      },
-      {
-        'customer': 'Atlanta Dental Supply, Inc.',
-        'arr': '19395',
-        'assets': [
-          'AWR - Booking GS',
-          'AWR - Deal/Forward Buy GS',
-          'AWR - Non-Stock GS',
-          'AWR - Plans GS',
-          'AWR - Projections GS',
-          'JDA Advanced Warehouse Replenishment GS'
-        ],
-        'theatre': 'NA MFG',
-        'industry': 'Wholesale/Distribution',
-        'is_cloud': 'N',
-        'chs': 92,
-        'class': 'class B'
-      }
-    ];
+
     public predictions: any;
+    public diseaseList;
     constructor(public restService: RestService, public router: Router) { }
     simulation:any
 
@@ -233,7 +106,7 @@ console.log(graph)
           width = +svg.attr("width"),
           height = +svg.attr("height");
 
-      var color = d3.scaleOrdinal();
+      var color = d3.scaleOrdinal(schemeCategory10);
 
       var nd;
       for (var i=0; i<graph.nodes.length; i++) {
@@ -657,9 +530,10 @@ svg.append("g")
           });
 
     }
-    onClick(){
-      console.log('hi')
-      this.router.navigate(['/diseases']);
-
+    onClick(diseaseName:string){
+      this.restService.getDiseaseList(diseaseName).subscribe((data)=>{
+        this.diseaseList = data;
+        console.log(this.diseaseList)
+      })
     }
 }
